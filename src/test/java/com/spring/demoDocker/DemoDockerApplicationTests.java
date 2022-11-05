4c09d7684624306f1230a8d42d20de11194b3273
VRralf <vrralf@gmail.com>
2022-11-05 02:22:26 -0300

    first commit

---
+++ src/test/java/com/spring/demoDocker/DemoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.demoDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
